1e0668e7b0791727cb8cba8a564b8f158d6c8998
Cartrell <zir0@sbcglobal.net>
2018-12-26 22:37:21 -0500

    Code cleanup

--- app/src/main/java/com/gameplaycoder/thunderjack/engine/BjBetSystem.java
+++ app/src/main/java/com/gameplaycoder/thunderjack/engine/BjBetSystem.java
@@ -2,7 +2,6 @@ package com.gameplaycoder.thunderjack.engine;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
