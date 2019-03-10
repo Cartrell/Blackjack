d400922e34625320d1e3305e2a2c47642fd52a2a
Cartrell <zir0@sbcglobal.net>
2019-03-10 13:20:31 -0400

    Removed unused code

--- app/src/main/java/com/gameplaycoder/thunderjack/engine/BjEngine.java
+++ app/src/main/java/com/gameplaycoder/thunderjack/engine/BjEngine.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.AsyncLayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BjEngine implements IBjEngine {
